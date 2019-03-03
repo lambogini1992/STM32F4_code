5c8359bcc8dee5975c2fb1b9bac53bdb720d6f0a
anhptvt1992 <lambogini1992@gmail.com>
2019-03-03 22:51:48 +0700

    This source code is used for MEM sensor L3GD20

--- mems_sensor/main.c
+++ mems_sensor/main.c
@@ -1,129 +1,102 @@
-#include <stdio.h>
-#include <string.h>
+
 #include "main.h"
-#include "l3gd20.h"
-
-
-typedef enum{
-  X_axis = 0,
-  Y_axis,
-  Z_axis
-}AXIS_ROTATION;
-
-
+#include "lib_l3gd20.h"
+#include "uart_string.h"
 SPI_HandleTypeDef hspi1;
-DMA_HandleTypeDef hdma_spi1_rx;
 
-UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
 
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_DMA_Init(void);
-static void MX_USART1_UART_Init(void);
 static void MX_SPI1_Init(void);
+static void MX_USART2_UART_Init(void);
+char data_pc[100];
+char data_recode[100];
+uint16_t len;
 
-static void read_data_mems(void);
-static void send_uart(AXIS_ROTATION axis,int16_t data);
-static void uart_string_send(char string[]);
-
-static L3GD20_data_out read_data_sensor;
+L3GD20_DATA_RECORD new_data_recode;
+L3GD20_DATA_RECORD old_data_recode;
 
 int main(void)
 {
-  int i = 0;
+
+  memset(&new_data_recode, 0, sizeof(L3GD20_DATA_RECORD));
+  memset(&old_data_recode, 0, sizeof(L3GD20_DATA_RECORD));
+
   HAL_Init();
+
   SystemClock_Config();
 
   MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_USART1_UART_Init();
   MX_SPI1_Init();
-
-	
-  if(L3GD20_Result_Error != L3GD20_Init(&hspi1, L3GD20_SCALE_250))
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  set_uart(&huart2);
+  L3GD20_Set_SPI(&hspi1);
+  if(L3GD20_ReadID() == I_AM_L3GD20)
   {
-    while(i != 10)
-    {
-      HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15);
-      i++;
-      HAL_Delay(100);
-    }
+    sprintf(data_pc, "Hello L3GD20 \r\n");
+    len = strlen(data_pc);
+    HAL_UART_Transmit_IT(&huart2, (uint8_t *)data_pc, len);
   }
-  memset(&read_data_sensor, 0, sizeof(L3GD20_data_out));
+  else
+  {
+    sprintf(data_pc, "Cannot read L3GD20 \r\n");
+    len = strlen(data_pc);
+    HAL_UART_Transmit_IT(&huart2, (uint8_t *)data_pc, len);
+  }
 
+  L3GD20_Init(0x000F);
+	HAL_Delay(1000);
+  /* USER CODE END 2 */
 
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
   while (1)
   {
     /* USER CODE END WHILE */
-    read_data_mems();
-    HAL_Delay(100);
+		
+    L3GD20_ReadXYZAngRate(&new_data_recode);
+    if(new_data_recode.X_data != old_data_recode.X_data)
+    {
+			memset(data_pc, 0, 100*sizeof(char));
+			len = 0;
+      sprintf(data_recode, "X %f\n",new_data_recode.X_data);
+      len = strlen(data_recode);
+      HAL_UART_Transmit_IT(&huart2, (uint8_t *)data_recode, len);
+      old_data_recode.X_data =new_data_recode.X_data;
+			HAL_Delay(100);
+    }
+    if(new_data_recode.Y_data != old_data_recode.Y_data)
+    {
+			memset(data_pc, 0, 100*sizeof(char));
+			len = 0;
+      sprintf(data_recode, "Y %f\n",new_data_recode.Y_data);
+      len = strlen(data_recode);
+      HAL_UART_Transmit_IT(&huart2, (uint8_t *)data_recode, len);
+      old_data_recode.Y_data =  new_data_recode.Y_data ;
+			HAL_Delay(100);
+    }
+    if(new_data_recode.Z_data != old_data_recode.Z_data)
+    {
+			memset(data_pc, 0, 100*sizeof(char));
+			len = 0;
+      sprintf(data_recode, "Z %f\n",new_data_recode.Z_data);
+      len = strlen(data_recode);
+      HAL_UART_Transmit_IT(&huart2, (uint8_t *)data_recode, len);
+      old_data_recode.Z_data = new_data_recode.Z_data;
+			HAL_Delay(100);
+    }
+    // HAL_Delay(500);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
 
-static void read_data_mems(void)
-{
-  L3GD20_data_out read_data_current;
-
-  L3GD20_Read(&read_data_current);
-
-  if(read_data_sensor.X_data != read_data_current.X_data)
-  {
-    read_data_sensor.X_data = read_data_current.X_data;
-    HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
-    // send_uart(X_axis,read_data_sensor.X_data);
-  }
-
-  if(read_data_sensor.Y_data != read_data_current.Y_data)
-  {
-    read_data_sensor.Y_data = read_data_current.Y_data;
-    HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);
-    // send_uart(Y_axis, read_data_sensor.Y_data);
-  }
-
-  if(read_data_sensor.Z_data != read_data_current.Z_data)
-  {
-    read_data_sensor.Z_data = read_data_current.Z_data;
-    HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14|GPIO_PIN_15);
-    // send_uart(Z_axis, read_data_sensor.Z_data);
-  }
-}
-
-static void send_uart(AXIS_ROTATION axis,int16_t data)
-{
-  char string[50];
-
-  memset(string, NULL, 50*sizeof(char));
-  switch(axis)
-  {
-    case X_axis:
-    sprintf(string, "X axis rotation: %i", data);
-    uart_string_send(string);
-
-    case Y_axis:
-    sprintf(string, "Y axis rotation: %i", data);
-    uart_string_send(string);
-
-    case Z_axis:
-    sprintf(string, "Z axis rotation: %i", data);
-    uart_string_send(string);
-  }
-}
-
-static void uart_string_send(char string[])
-{
-  uint8_t data;
-  data = 0;
-  while(*string != NULL)
-  {
-    HAL_UART_Transmit_IT(&huart1, (uint8_t *)string, 1);
-    // HAL_UART_Transmit_IT(&huart1, &data, 1);
-    data++;
-    string++;
-  }
-}
-
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -140,7 +113,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 200;
+  RCC_OscInitStruct.PLL.PLLN = 192;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
@@ -185,7 +158,7 @@ static void MX_SPI1_Init(void)
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
@@ -201,50 +174,35 @@ static void MX_SPI1_Init(void)
 }
 
 /**
-  * @brief USART1 Initialization Function
+  * @brief USART2 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_USART1_UART_Init(void)
+static void MX_USART2_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART1_Init 0 */
+  /* USER CODE BEGIN USART2_Init 0 */
 
-  /* USER CODE END USART1_Init 0 */
+  /* USER CODE END USART2_Init 0 */
 
-  /* USER CODE BEGIN USART1_Init 1 */
+  /* USER CODE BEGIN USART2_Init 1 */
 
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 19200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART1_Init 2 */
+  /* USER CODE BEGIN USART2_Init 2 */
 
-  /* USER CODE END USART1_Init 2 */
-
-}
-
-/** 
-  * Enable DMA controller clock
-  */
-static void MX_DMA_Init(void) 
-{
-  /* DMA controller clock enable */
-  __HAL_RCC_DMA2_CLK_ENABLE();
-
-  /* DMA interrupt init */
-  /* DMA2_Stream0_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+  /* USER CODE END USART2_Init 2 */
 
 }
 
@@ -267,7 +225,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_SET);
 
   /*Configure GPIO pin : PE3 */
   GPIO_InitStruct.Pin = GPIO_PIN_3;
@@ -276,8 +234,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PD12 PD13 PD14 PD15 */
-  GPIO_InitStruct.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  /*Configure GPIO pins : PD14 PD15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
